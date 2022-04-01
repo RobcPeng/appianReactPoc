7802ee475bbb25adc99458feae863c770aa689b5
peng <robertjustinianpeng@gmail.com>
2022-04-01 14:44:37 -0600

    adding graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import "./App.css";
+import API from "@aws-amplify/api";
+import React from "react";
+import ReactDOM from "react-dom";
+import { VictoryBar } from "victory";
 
 function App() {
+  const data = [
+    { quarter: 1, earnings: 13000 },
+    { quarter: 2, earnings: 16500 },
+    { quarter: 3, earnings: 14250 },
+    { quarter: 4, earnings: 19000 },
+  ];
   return (
     <div className="App">
+      <VictoryBar data={data} x="quarter" y="earnings" />
       <button onClick={() => console.log("hi")}>hi</button>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import awsExports from "./aws-exports";
+import API from "@aws-amplify/api";
 
 ReactDOM.render(
   <React.StrictMode>
