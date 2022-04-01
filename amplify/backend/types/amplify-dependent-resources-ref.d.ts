d8376c802ec8053e108dec16ec7a8f91a476d7ba
peng <robertjustinianpeng@gmail.com>
2022-04-01 17:46:06 -0600

    adding auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "appianreactpocf569fd23": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,61 @@ import API from "@aws-amplify/api";
 import React from "react";
 import ReactDOM from "react-dom";
 import { VictoryBar } from "victory";
+import Amplify from "aws-amplify";
+import {
+  AmplifyProvider,
+  Authenticator,
+  Button,
+  Flex,
+  Image,
+  Text,
+  Theme,
+  View,
+} from "@aws-amplify/ui-react";
+import aws_exports from "./aws-exports";
+
+Amplify.configure(aws_exports);
 
 function App() {
-  const data = [
-    { quarter: 1, earnings: 13000 },
-    { quarter: 2, earnings: 16500 },
-    { quarter: 3, earnings: 14250 },
-    { quarter: 4, earnings: 19000 },
-  ];
   return (
-    <div className="App">
-      <VictoryBar data={data} x="quarter" y="earnings" />
-      <button onClick={() => console.log("hi")}>hi</button>
-    </div>
+    <AmplifyProvider theme={theme}>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <Flex
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
+            alignContent="flex-start"
+            wrap="nowrap"
+            gap="1rem"
+            textAlign="center"
+          >
+            {user && (
+              <View width="100%">
+                <Text>Hello {user.username}</Text>
+                <Button onClick={signOut}>
+                  <Text>Sign Out</Text>
+                </Button>
+              </View>
+            )}
+
+            <View width="100%"></View>
+          </Flex>
+        )}
+      </Authenticator>
+    </AmplifyProvider>
   );
 }
+
+const theme: Theme = {
+  name: "cra-my-theme",
+  tokens: {
+    colors: {
+      font: {
+        primary: { value: "#008080" },
+      },
+    },
+  },
+};
 
 export default App;
